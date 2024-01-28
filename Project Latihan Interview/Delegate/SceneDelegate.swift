0a4fb391f959ebbeac99df11428f70826d020f91
Arya <aryailham05@gmail.com>
2024-01-28 10:12:34 +0700

    Update SceneDelegate.swift

--- Project Latihan Interview/Delegate/SceneDelegate.swift
+++ Project Latihan Interview/Delegate/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -15,6 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        let config = Realm.Configuration()
+        Realm.Configuration.defaultConfiguration = config
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let vc = ViewController.instantiate()
         let nav = UINavigationController(rootViewController: vc)
